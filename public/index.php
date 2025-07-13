1436de027c865b4181b3cd79306aaa45373f5a9c
inti-menendez <intimenendez2004@gmail.com>
2025-07-13 18:01:51 -0400

    Add .htaccess for URL rewriting and include requestController.js in index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<script src="js/requestController.js"></script>
 <?php
-
+session_start();
 require_once '../core/launcher.php';
 ?>
 
