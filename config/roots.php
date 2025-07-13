8397f013d397d00ebde9d58e02088918f6d23845
inti-menendez <intimenendez2004@gmail.com>
2025-07-13 18:02:14 -0400

    Update PATH_CONFIG to point to the correct configuration directory

--- config/roots.php
+++ config/roots.php
@@ -1,7 +1,7 @@
 <?php
 
 define('PATH_APP', '../app/');
-define('PATH_CONFIG', '../config/autoloader/');
+define('PATH_CONFIG', '../config/');
 define('PATH_CORE', '../core/');
 define('PATH_PUBLIC', '../public/');
 define('PATH_ROUTES', '../config/routes/');
