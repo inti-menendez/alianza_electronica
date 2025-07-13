881d25004940fafae2eaeaefec9b849225818563
inti-menendez <intimenendez2004@gmail.com>
2025-07-13 16:19:28 -0400

    Add launcher script for routing and request handling

---
+++ core/launcher.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../config/roots.php';
+require_once '../core/Autoloader/Autoloader.php';
+
+Autoloader::register();
+
+$paths = scandir(PATH_ROUTES);
+
+foreach ($paths as $file) {
+    $filePath = realpath(PATH_ROUTES . $file);
+    if (is_file($filePath)) {
+        require $filePath;
+    }
+}
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+Router::dispatch($uri, $method);
