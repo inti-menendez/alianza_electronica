882da6386b09e47821e11e5a7dfa1e81c7383fdc
inti-menendez <intimenendez2004@gmail.com>
2025-07-13 18:45:37 -0400

    Update script reference in index.php to use form.core.js as a module

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-<script src="js/requestController.js"></script>
+<script src="js/form.core.js" type="module"></script>
 <?php
 session_start();
 require_once '../core/launcher.php';
