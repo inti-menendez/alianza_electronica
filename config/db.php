d634a14d9a36a7b29328e05ee9a8317fcb6b1678
inti-menendez <intimenendez2004@gmail.com>
2025-07-13 16:51:09 -0400

    Refactor database connection method to use static properties

--- config/db.php
+++ config/db.php
@@ -19,15 +19,10 @@ class db {
 
     }
 
-    private function getDsn()
-    {
-        return $this->dsn;
-    }
-
-    public function connect()
+    public static function connect()
     {
         try {
-            $pdo = new PDO($this->getDsn(), $this->username, $this->password);
+            $pdo = new PDO(self::$dsn, self::$username, self::$password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $pdo;
