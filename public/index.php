e8876128d97c92b42a1c15c310fa4bd477d6f78d
inti-menendez <intimenendez2004@gmail.com>
2025-07-14 22:44:47 -0400

    Add utility styles and route function to index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,24 @@
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <script src="js/form.core.js" type="module"></script>
+<style>
+    @layer utilities {
+        .primary {
+            color: #076AAE;
+            /* Rojo */
+        }
+        .secondary {
+            color: #0097D8;
+            /* Azul */
+        }
+        .grey {
+            color: #4d4d4d;
+        }
+    }
+</style>
 <?php
+function route($path = '') {
+  return PATH_PUBLIC . trim($path, '/');
+}
 session_start();
 require_once '../core/launcher.php';
 ?>
-
-
-
