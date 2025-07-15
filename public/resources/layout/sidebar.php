479277ccae9f8f04d218e4ec030c0ecf78e38fe5
inti-menendez <intimenendez2004@gmail.com>
2025-07-14 22:59:32 -0400

    Update sidebar links to use relative paths for navigation

--- public/resources/layout/sidebar.php
+++ public/resources/layout/sidebar.php
@@ -1,8 +1,8 @@
 <aside class="bg-white w-64 p-4 border-r hidden md:block">
   <ul class="space-y-4">
-    <li><a href="/dashboard" class="block text-blue-600 font-semibold">Dashboard</a></li>
-    <li><a href="/clientes" class="block hover:text-blue-500">Clientes</a></li>
-    <li><a href="/equipos" class="block hover:text-blue-500">Equipos</a></li>
-    <li><a href="/tareas" class="block hover:text-blue-500">Tareas</a></li>
-  </ul>
+    <li><a href="home" class="block text-blue-600 font-semibold">Dashboard</a></li>
+    <li><a href="customers" class="block hover:text-blue-500">Clientes</a></li>
+    <li><a href="devices" class="block hover:text-blue-500">Equipos</a></li>
+    <li><a href="tasks" class="block hover:text-blue-500">Tareas</a></li>
+  </ul> 
 </aside>
