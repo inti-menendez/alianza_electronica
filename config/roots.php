cf6153c390230ab9e9c6ebc3b7fdb095d11afb37
inti-menendez <intimenendez2004@gmail.com>
2025-07-15 23:37:15 -0400

    Add definition for ERROR_CODES_DIR in roots.php

--- config/roots.php
+++ config/roots.php
@@ -5,6 +5,7 @@ define('PATH_CONFIG', '../config/');
 define('PATH_CORE', '../core/');
 define('PATH_PUBLIC', '../public/');
 define('PATH_ROUTES', '../config/routes/');
+define('ERROR_CODES_DIR', PATH_PUBLIC . 'resources/error_codes');
 
 ?>
 
