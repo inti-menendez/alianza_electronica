a54cb8de08689bdcc88371d3e29f30330f515f0e
inti-menendez <intimenendez2004@gmail.com>
2025-07-25 23:13:19 -0400

    add bg-white to register customer section

--- app/customers/views/index.php
+++ app/customers/views/index.php
@@ -29,9 +29,10 @@ $ultimos = array_slice($clientes, 0, 3);
 </div>
 
 <div x-data="{ tab: 'cards' }" class="mb-6">
-    <div class="flex border-b border-gray-200 space-x-4">
-        <button @click="tab = 'cards'" :class="tab === 'cards' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-600'"
-            class="py-2 px-3 border-b-2 text-sm font-medium transition hover:text-blue-600 flex items-center gap-1">
+    <div class="bg-white rounded-lg  p-4">
+        <div class="flex border-b border-gray-200 space-x-4">
+            <button @click="tab = 'cards'" :class="tab === 'cards' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-600'"
+                class="py-2 px-3 border-b-2 text-sm font-medium transition hover:text-blue-600 flex items-center gap-1">
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
@@ -61,7 +62,7 @@ $ultimos = array_slice($clientes, 0, 3);
             </div>
         </div>
     </div>
-
+</div>
 
 </div>
 <script>
